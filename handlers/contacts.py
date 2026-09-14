19485bea96b8f607b2bad7355dd592ddbddc6e2d
MishaBabala <mihajlobabala94@gmail.com>
2026-09-14 23:37:52 +0300

    Updated BeautyBot

--- handlers/contacts.py
+++ handlers/contacts.py
@@ -7,13 +7,13 @@ router = Router()
 @router.message(lambda message: message.text == "☎️ Контакти")
 async def contacts(message: Message):
     await message.answer(
-        "☎️ Телефон:\n+380XXXXXXXXX"
+        "☎️ Телефон:\n+380992058456\n\n"
     )
 
 
-@router.message(lambda message: message.text == "📍 Адреса")
+@router.message(lambda message: message.text == "📍 вулиця Олександра Довженка, 25Б, Івано-Франківськ, Івано-Франківська область, 76000 ")
 async def address(message: Message):
     await message.answer(
         "📍 м. Івано-Франківськ\n"
-        "вул. __________"
+        "вул. Олександра Довженка, 25Б\n"
     )
